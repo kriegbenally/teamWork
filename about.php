fce4b5a2574cfee55ed2aeaa6c7692190849f1b6
Krieg Benally <kriegbenally@gmail.com>
2016-10-12 15:21:48 -0600

    added jumbotron added index.css added about.css

--- about.php
+++ about.php
@@ -4,10 +4,11 @@
     <meta charset="utf-8">
     <title></title>
     <?php require_once("lib/headUtils.php"); ?>
+    <link rel="stylesheet" href="lib/css/about.css">
   </head>
   <body>
 
-    <?php require_once("lib/partails/navBar.php"); ?>
+    <?php require_once("lib/partials/navBar.php"); ?>
 
 
   </body>
--- index.php
+++ index.php
@@ -4,10 +4,70 @@
     <meta charset="utf-8">
     <title></title>
     <?php require_once("lib/headUtils.php"); ?>
+    <link rel="stylesheet" href="lib/css/index.css">
   </head>
   <body>
 
-    <?php require_once("lib/partails/navBar.php"); ?>
+    <?php require_once("lib/partials/navBar.php"); ?>
+
+    <div class="container">
+      <div class="jumbotron">
+        <h1>This is my Index page.</h1>
+        <p>Welcome.  You are more than welcome to browse around</p>
+        <div class="row">
+          <div class="col-md-3 col-md-offset-9">
+            <button class="btn-lg">LEARN MORE!</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Headline</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+            in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+             officia deserunt mollit anim id est laborum.
+           </p>
+           <div class="col-md-6 col-md-offset-6">
+             <button class="btn-sm btn-danger">Click to learn more.</button>
+           </div>
+        </div>
+        <div class="col-md-4">
+          <h2>Headline</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+            in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+             officia deserunt mollit anim id est laborum.
+           </p>
+           <div class="col-md-6 col-md-offset-6">
+             <button class="btn-sm btn-success">Click to learn more.</button>
+           </div>
+        </div>
+        <div class="col-md-4">
+          <h2>Headline</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+            in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+             officia deserunt mollit anim id est laborum.
+           </p>
+           <div class="col-md-6 col-md-offset-6">
+             <button class="btn-sm btn-danger">Click to learn more.</button>
+           </div>
+        </div>
+      </div>
+    </div>
 
 
   </body>
